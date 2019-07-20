f8e96de6676aa66d0b844ed322f92c538c4df02c
prati1 <pratiksha.shrestha97@gmail.com>
2019-07-21 02:50:19 +0545

    list doctors

--- backend/doctors_list/index.js
+++ backend/doctors_list/index.js
@@ -5,48 +5,59 @@ const doctorsListTable = process.env.doctors_table;
  
 exports.handler = (event, context, callback) => {
     console.info("Recieved Event:", JSON.stringify(event, null, 2));
-
+    context.callbackWaitsForEmptyEventLoop = true;
     const params = {
         TableName: doctorsListTable
       };
 
-      dynamoDbClient.scan(params).eachPage((err, data) => {
+      dynamoDbClient.scan(params, (err, data) => {
         if (err) {
             console.log(err, err.stack);
             callback(null, {
+                'isBase64Encoded': true|false,
                 statusCode: '500',
-                messageCode: 'itemListError',
+                // messageCode: 'itemListError',
+                headers: {
+                    "Access-Control-Allow-Origin": `*`,
+                    "Access-Control-Allow-Credentials": true,
+                },
                 body: err
             });
         } else {
+            console.log('data', data);
             if (data != null) {
-                const finalResponse = {
-                    statusCode: '200',
-                    headers: {
-                    "Access-Control-Allow-Origin": `*`,
-                    "Access-Control-Allow-Credentials": true,
-                    },
-                    message: 'Doctors list obtained',
-                    body: data.Items,
-                }
-                callback(null, {
-                    statusCode: '200',
-                    body: JSON.stringify(finalResponse)
+                let c = 0;
+                let result = {};
+                data.Items.forEach(function(item) {
+                    console.log(item); 
+                    result[c] = item; 
+                    c++
                 });
-            } else {
+
                 const finalResponse = {
+                    'isBase64Encoded': true|false,
                     statusCode: '200',
                     headers: {
                         "Access-Control-Allow-Origin": `*`,
                         "Access-Control-Allow-Credentials": true,
                     },
-                    message: 'noDoctorsFound',
-                    body: data
-                }
-                callback(null, {
+                    // message: 'Doctors list obtained',
+                    body: JSON.stringify(result)
+                };
+                console.log(finalResponse);
+                callback(null, finalResponse);
+            } else {
+                const finalResponse = {
+                    'isBase64Encoded': true|false,
                     statusCode: '200',
-                    body: JSON.stringify(finalResponse)
-                });
+                    headers: {
+                        "Access-Control-Allow-Origin": `*`,
+                        "Access-Control-Allow-Credentials": true,
+                    },
+                    // message: 'noDoctorsFound',
+                    body: JSON.stringify(data)
+                }
+                callback(null, finalResponse);
             }
 
         }
