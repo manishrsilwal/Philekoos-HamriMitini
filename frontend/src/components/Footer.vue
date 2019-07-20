bdc6e51ad2114d919fabccf32bd24c797f9d0893
Manish Raj Silwal <manishraj.silwal@student.ku.edu.np>
2019-07-20 20:49:59 +0545

    Index page sections layout with dummy data

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+<div id="app">
+  <div class="contents footer">
+    <div class="row">
+      <!-- Copyright Text -->
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: "Footer",
+  components:{
+  },
+
+  data() {
+    return {
+    }
+  },
+
+  mounted () {
+    // axios
+    //   .get('https://75anpql3b4.execute-api.ap-southeast-1.amazonaws.com/development/list-contact')
+    //   .then(response => (this.api_data = response))
+  },
+
+  // methods:{
+  //   ...mapActions(['setToken']),
+
+
+  // }
+};
+</script>
+
+<style lang="scss" scoped>
+  @import "../css/footer.scss";
+
+</style>
