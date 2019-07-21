24da49bd205e2bc7f19a16c46e9fbac80a070e1e
Manish Raj Silwal <manishraj.silwal@gmail.com>
2019-07-21 06:34:07 +0545

    Logo Added

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
 <div id="app" class="header">
   <b-navbar toggleable="lg" type="light" fixed="top">
-    <b-navbar-brand to="/">HamriMitini</b-navbar-brand>
+    <b-navbar-brand to="/"><img src="logo.png" /></b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
